fab84a7f09097e5b763f05d583fc63ff126be93a
Rishiraj Singh <rishiraj.25mei10079@vitbhopal.ac.in>
2025-11-25 18:44:49 +0530

    Fix formatting of constants in config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ MASTER_PASS_FILE = "data/master.txt"
 VOICEPRINT_FILE = "data/voiceprint.npy"
 DB_FILE = "data/vault.db"
 
-MFCC_N_MFCC = 20     # number of MFCC features
-SIMILARITY_THRESHOLD = 0.92  # similarity threshold
+MFCC_N_MFCC = 20     
+SIMILARITY_THRESHOLD = 0.92 
